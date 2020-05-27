1cf62e7351a2c82e5da0a66aef999f4755762043
rosshandler <ii236@cam.ac.uk>
2020-05-27 16:48:17 +0100

    include source_functions.R

--- analysis/4_normalisation/normalisation.R
+++ analysis/4_normalisation/normalisation.R
@@ -20,59 +20,7 @@ if (parallel == TRUE){
 
 path2data  <- "/hps/research1/marioni/ivan/EmbryoTimeCourse2020/data/"
 
-load_data2020 = function(normalise = TRUE, remove_doublets = FALSE, remove_stripped = FALSE, load_corrected = FALSE){
-  
-  if(load_corrected & (!remove_doublets | !remove_stripped)){
-    message("Using corrected PCs, also removing doublets + stripped now.")
-    remove_doublets = TRUE
-    remove_stripped = TRUE
-  }
-  
-  require(scran)
-  require(scater)
-  require(SingleCellExperiment)
-  require(Matrix)
-  
-  path2data  <- "/hps/research1/marioni/ivan/EmbryoTimeCourse2020/data/"
-  counts     <- readRDS(paste0(path2data,"raw_counts_qc.rds"))
-  genes      <- read.table(paste0(path2data, "genes.tsv"), stringsAsFactors = F)
-  meta       <- read.table(paste0(path2data, "meta.tab"), header = TRUE, sep = "\t",
-    stringsAsFactors = FALSE, comment.char = "$")
-  
-  rownames(counts) = genes[,1] #ensembl
-  colnames(counts) = meta$cell
-
-  sce = SingleCellExperiment(assays = list("counts" = counts))
-  
-  if(normalise){
-    sfs = read.table(paste0(path2data, "sizefactors.tab"), stringsAsFactors = F)[,1]
-    sizeFactors(sce) = sfs
-    sce = scater::normalize(sce)
-  }
-  
-  if(remove_doublets){
-    sce = scater::normalize(sce[,!meta$doublet])
-    meta = meta[!meta$doublet,]
-  }
-  
-  if(remove_stripped){
-    sce = scater::normalize(sce[,!meta$stripped])
-    meta = meta[!meta$stripped, ]
-  }
-  
-  if(load_corrected){
-    corrected = readRDS(paste0(path2data, "corrected_pcas.rds"))
-    assign("corrected", corrected, envir = .GlobalEnv)  
-  }
-    
-  
-  assign("genes", genes, envir = .GlobalEnv)
-  assign("meta", meta, envir = .GlobalEnv)
-  assign("sce", sce, envir = .GlobalEnv)
-  
-  
-  invisible(0)
-}
+source("/hps/research1/marioni/ivan/EmbryoTimeCourse2020/core_scripts/core_functions.R")
 
 load_data2020(normalise = FALSE)
 
