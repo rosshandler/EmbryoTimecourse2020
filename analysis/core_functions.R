fd655edcaeeb9250ec8673d43b3bc33b41e46042
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2021-12-16 22:53:48 +0000

    Update core_functions.R

--- analysis/core_functions.R
+++ analysis/core_functions.R
@@ -23,6 +23,9 @@ setParallel <- function(ncores = 1){
 # Colour transparency
 ac <- function(x, alpha=1, ...) { y <- adjustcolor(x, alpha.f=alpha, ...); names(y) <- names(x); return(y)}
 
+# Biomart reference
+mouse_ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
+
 load_data2020 = function(normalise = TRUE, remove_doublets = FALSE, remove_stripped = FALSE, load_corrected = FALSE){
   
   if(load_corrected & (!remove_doublets | !remove_stripped)){
@@ -75,3 +78,38 @@ load_data2020 = function(normalise = TRUE, remove_doublets = FALSE, remove_strip
 }
 
 
+
+sceIntegrateEmbryosStep1 <- function(path2sce_atlas, path2sce_extension, path2out){
+
+  load(path2atlas)
+  atlas_sce  <- sce
+  atlas_meta <- meta
+
+  load(path2extension)
+  ext_sce    <- sce
+  ext_meta   <- meta
+  
+  #mouse_ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
+ 
+  shared_genes  <- intersect(rownames(atlas_sce), rownames(ext_sce))
+
+  atlas_sce  <- atlas_sce[shared_genes, ]
+  ext_sce    <- ext_sce[shared_genes, ]
+
+  #prevent duplicate cell names
+  colnames(ext_sce) <- paste0("ext_", colnames(ext_sce))
+  
+  writeLines(shared_genes, con = paste0(path2out, "shared_genes.txt"))
+  
+  gene_map <- getBM(attributes=c("ensembl_gene_id", "mgi_symbol"),
+    filters = "ensembl_gene_id", values = shared_genes, mart = mouse_ensembl)
+  
+  write.table(gene_map, file = paste0(path2out,  "shared_genesyms.txt"), row.names = FALSE)
+
+  counts    <- cbind(counts(atlas_sce), counts(ext_sce))
+  logcounts <- cbind(Matrix(logcounts(atlas_sce), sparse=TRUE), logcounts(ext_sce))
+ 
+  saveRDS(counts, paste0(path2out, "integrated_counts_sparse.rds"))
+  saveRDS(logcounts, paste0(path2out, "integrated_logcounts_sparse.rds"))
+
+}
